73aca9e57c51fbb60440b2be4cf253c79b1c2914
Muhamad Meiko Triputra <90701128+meikotrip@users.noreply.github.com>
2022-05-26 11:09:53 +0700

    Create array.js

---
+++ array/array.js
@@ -0,0 +1,22 @@
+var foods = [];
+
+function daftarArr() {
+    document.getElementById("foodies").innerHTML =foods;
+}
+
+function addArray() {
+    var food = prompt("Masukkan Makanan atau minuman favorit Anda !");
+    foods[foods.length] = food;
+    document.getElementById("foodies").innerHTML =foods;
+}
+
+function removeArr() {
+    if (foods.length == 1) {
+        foods.splice(0,1);
+    }
+    else {
+        var num = prompt("Item nomor berapa yang ingin dihapus ?");
+        foods.splice(num-1, num);
+    }
+    document.getElementById("foodies").innerHTML =foods;
+}
